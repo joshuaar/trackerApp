44b7584de88d09d6f4bf8d8db5a76eee236fd445
Josh <joshamosricher@gmail.com>
2013-06-25 14:16:08 -0700

    Finished api basics, may be able to get rid of mongo and store everything in memory. Would be easier to deploy this way

--- app.js
+++ app.js
@@ -33,67 +33,30 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-//Routes for polling
-var longPollTimeout = 30000
-var connected = {}
 
-//adds remotes to connected object, to be accessed by others
-app.get('/p/:dev/listen',function(req,res){
-    //console.log(connected[req.params.dev] == null)
-    if(connected[req.params.dev]){
 
-        //connected[req.params.dev]["res"].send(200) //deal with old request
-        //connected[req.params.dev] = {res:res,time:new Date().getTime()} //Put new request in connected dict.
-        res.send(403)
-        console.log(connected)
-    } else {
-        var time = new Date().getTime()
-        connected[req.params.dev] = {res:res,time:time} //Put new request in connected dict
-        //res.send("hello")
-        setTimeout(function(){   //After a certain time, remove connection from dict of active connections
-            try{
-               // console.log(connected[req.params.dev]["time"])
-               // console.log(time)
-                if(connected[req.params.dev]["time"] == time){
-                    connected[req.params.dev]["res"].send(200)
-                    connected[req.params.dev] = null
-                }
-            } catch (exception) {
-                console.log("request expired")
-            }
-        },longPollTimeout)
-    }
-})
+//Routes for listening and sending messages to registered devices
 
-app.get('/m/:dev/:msg',function(req,res){
-    console.log(req.params.msg)
-    if(connected[req.params.dev]){
-        connected[req.params.dev]["res"].send("Greetings from elsewhere: "+req.params.msg)
-        connected[req.params.dev] = null
-        res.send(200)
-    }
-    res.send("Device not connected")
-
-})
+app.get('/p/:dev',mongoCon.longPollHandler)
+setInterval(function(){mongoCon.rmOld(40000)},40000)
+app.post('/p/:dev',mongoCon.devPostHandler)
 
 //Routes for adding, getting, deleting (potentially) connectable devices
 
 app.get('/u/:uid', function(req,res){
     mongoCon.getIPs(req.params.uid,function(ips){
-        var ipTxt = new Array()
-        for(var i=0;i<ips.length;i++){
-            ipTxt[i]=ips[i].ip
-        }
         res.send(ips)
     })
-
 })
 
 app.post('/u/:uid',function(req,res){
     var extern = req.connection.remoteAddress
     var intern = req.body.IP
-    mongoCon.insertIP(extern,intern,req.params.uid)
-    res.send({extern:extern,intern:intern})
+    var uid = req.params.uid
+    mongoCon.insertIP(extern,intern,req.params.uid, function(devID){
+
+        res.send(devID)
+    })
 })
 
 app.delete('/u/:uid',function(req,res){
--- mongoCon.js
+++ mongoCon.js
@@ -1,13 +1,15 @@
 
 var mongoose = require('mongoose');
 var dbName = 'mongodb://localhost/test';
+var crypto = require('crypto')
 mongoose.connect(dbName)
 
 //Define schema
 var ipSchema = mongoose.Schema({
 	ip: {external:String,local:String},
-    time:String,
-	uid: String
+    time:Number,
+	uid: String,
+    devID: String
 });
 
 
@@ -18,30 +20,101 @@ ipSchema.methods.speak = function() {
 var IP = mongoose.model('IP',ipSchema)
 
 //Insert an IP address with uid into the DB
-var insertIP = function(ip,ipLocal, uid) {
+var insertIP = function(ip,ipLocal, uid, callback) {
 	console.log("Creating new IP address Entry for "+ uid)
-    var entry = {ip:{external:ip,local:ipLocal}, uid:uid, time:new Date().getTime()}
-    var uniqueID = {ip:{external:ip,local:ipLocal}, uid:uid} //user id, local and external IPs uniquely ID a device on network
-	var ipEntry = new IP(entry)
-    IP.find(uniqueID,function(err,ips){
-        //console.log(ips)
-        if(err) throw err
-        if(ips.length == 0){
-            console.log("Does not exist, creating new entry")
-            ipEntry.save(function(err, ipEntry){//The function is called, this is to check errors
-                if(err) throw err;
-                ipEntry.speak();
-            });
-        }
-        else {
-            console.log("Record exists, updating timestamp")
-            IP.update(uniqueID,entry,function(err, numAffected){
-                if(err) throw err
-                console.log("updated "+numAffected+" records")
-            })
-        }
+    crypto.randomBytes(10,function(ex,buf){    //generate random dev id
+        var devID = buf.toString('hex')
+        var entry = {ip:{external:ip,local:ipLocal}, uid:uid, time:new Date().getTime()}
+        var uniqueID = {ip:{external:ip,local:ipLocal}, uid:uid} //user id, local and external IPs uniquely ID a device on network
+        IP.find(uniqueID,function(err,ips){
+
+            if(err) throw err
+            if(ips.length == 0){
+                console.log("Does not exist, creating new entry")
+                entry["devID"] = devID
+                var ipEntry = new IP(entry)
+                ipEntry.save(function(err, ipEntry){//The function is called, this is to check errors
+                    if(err) throw err;
+                    ipEntry.speak();
+                    alertUID(uid,devID)
+                    callback(devID)
+                });
+            }
+            else {
+                console.log("Record exists, updating timestamp")
+                IP.update(uniqueID,entry,function(err, numAffected){
+                    if(err) throw err
+                    console.log("updated "+numAffected+" records")
+                    console.log(ips)
+                    callback(ips[0].devID)//give existing devID
+                })
+            }
+        })
     })
 };
+
+//Alerts all connectable devices that a new device has come online
+var alertUID = function(uid,devID){
+    IP.find({uid:uid},function(err,records){
+        if(err) throw err
+        for(var i=0;i<records.length;i++){
+            var curdev = records[i].devID
+            if(connected[curdev]){
+                if(curdev != devID){
+                    connected[curdev]["res"].send(":NeW:"+uid+":"+devID)
+                    connected[curdev] = null
+                }
+            }
+        }
+    })
+}
+
+var longPollTimeout = 30000
+var connected = {}
+var longPollHandler = function(req,res){
+
+    if(connected[req.params.dev]){
+
+        res.send(403)
+        console.log(connected)
+    } else {
+        isRegisteredDevice(req,function(err){
+            if(err){
+                res.send(404)//Device was not found registered
+            }
+            //What to do if the listening device is registered (everything is going well)
+            else{
+                var time = new Date().getTime()
+                connected[req.params.dev] = {res:res,time:time} //Put new request in connected dict
+
+                updateDevTimestamp(req.params.dev,time)//update timestamp in DB
+
+                setTimeout(function(){   //After a certain time, remove connection from dict of active connections
+                    try{
+
+                        if(connected[req.params.dev]["time"] == time){
+                            connected[req.params.dev]["res"].send(200)
+                            connected[req.params.dev] = null
+                        }
+                    } catch (exception) {
+                        console.log("request expired")
+                    }
+                },longPollTimeout)
+            }
+        })
+    }
+}
+
+var devPostHandler = function(req,res){
+    if(connected[req.params.dev]){
+        console.log(req.body)
+        connected[req.params.dev]["res"].send(JSON.stringify(req.body))
+        connected[req.params.dev] = null
+        res.send(200)
+    }
+    res.send(404)
+
+}
 
 //get the IPs associated with a user ID
 var getIPs = function(uid, callback) {
@@ -55,6 +128,40 @@ var getIPs = function(uid, callback) {
 		callback(ips)
 	})
 }
+//fires callback if device is registered, else err
+var isRegisteredDevice = function(req, callback) {
+    var devID = req.params.dev
+    IP.find({devID:devID}, function(err,devs) {
+        if(devs.length > 0){
+            var record = devs[0]
+            var ipOnRecord = record["ip"]["external"]
+            if(ipOnRecord == req.connection.remoteAddress){
+                callback(0)
+            }
+            else{
+                callback(new Error("Device IP does not match request"))
+            }
+
+        }
+        else {
+            callback(new Error("Device not found"))
+        }
+    })
+}
+
+var getDev = function(uid, localIP, externalIP, callback) {
+    IP.find({uid:uid,ip:{external:externalIP,local:localIP}},function(err,results){
+        if(err) throw err
+        callback(results)
+    })
+}
+
+var updateDevTimestamp = function(devID,time) {
+    IP.update({devID:devID},{time:time},function(err,numAffected){
+        if(err) throw err
+        console.log(numAffected+" timestamp records updated")
+    })
+}
 
 var clearUID = function(uid) {
 	IP.find({uid: uid}, function(err,ips){
@@ -66,11 +173,24 @@ var clearUID = function(uid) {
 	});
 };
 
+var rmOld = function(timeout) {
+    cutoffTime = new Date().getTime() - timeout
+    IP.find({time: {$lte:cutoffTime}},function(err,ips){
+        for(var i = 0; i < ips.length; i++){
+            ips[i].remove()
+        }
+        console.log("cleaned up "+ips.length+" records")
+    })
+}
+
 //Export the functions
 exports.insertIP = insertIP;
 exports.getIPs = getIPs
 exports.clearUID = clearUID
-
+exports.updateDevTimestamp = updateDevTimestamp
+exports.rmOld = rmOld
+exports.longPollHandler = longPollHandler
+exports.devPostHandler = devPostHandler
 //clearUID("Default")
 //insertIP("192.168.1.123", "Default")
 //getIPs("Default",console.log)
